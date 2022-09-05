2d8e583c4c0d6a9024f3e3ed6df0bd4a2c0a12f2
Joshua Mulloy <josh@mulloy.ca>
2022-09-05 19:58:09 -0400

    added some whitespace

--- src/server/router/account.ts
+++ src/server/router/account.ts
@@ -2,7 +2,7 @@ import { createProtectedRouter } from "./protected-router";
 
 // Example router with queries that can only be hit if the user requesting is signed in
 export const accountRouter = createProtectedRouter()
-.query("getCurrent", {
+  .query("getCurrent", {
   async resolve({ ctx }) {
     return ctx.prisma.account.findUnique({
       where: {
@@ -10,4 +10,4 @@ export const accountRouter = createProtectedRouter()
       }
     });
   },
-});
+  });
