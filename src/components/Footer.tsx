2b2b5a922be9dcfac72451349414b799d6285168
Joshua Mulloy <josh@mulloy.ca>
2022-09-18 00:06:29 -0400

    Fixing Errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,14 @@ export default function Footer() {
   return (
     <footer className="p-4 bg-stone-900 shadow md:flex md:items-center md:justify-between dark:bg-gray-800 align-bottom hidden">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        Based on the book "1001 Albums You Must Hear Before You Die"
+        Based on the book &quot;1001 Albums You Must Hear Before You Die&quot;
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
           <a
             href="https://1001albumsgenerator.com"
             target="_blank"
+            rel="noreferrer"
             className="mr-4 hover:underline md:mr-6"
           >
             Inspiration
@@ -20,6 +21,7 @@ export default function Footer() {
           <a
             href="https://github.com/mulloyj/daily-album"
             target="_blank"
+            rel="noreferrer"
             className="mr-4 hover:underline md:mr-6"
           >
             GitHub
